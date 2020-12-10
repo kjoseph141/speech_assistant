fb0545bd4410fd787482291a406186b0aa9daa14
Kevin Joseph <68094449+kjoseph141@users.noreply.github.com>
2020-12-10 18:00:20 -0500

    Added gtts feature for speech and added commands

--- main.py
+++ main.py
@@ -2,68 +2,97 @@ import speech_recognition as sr
 import time
 from time import ctime
 import webbrowser
-import pyttsx3 as tts 
+import pyttsx3 as tts
+import playsound
+import os
+import random
+from gtts import gTTS
 
 
 rec = sr.Recognizer()
-engine = tts.init()
-voice_selection = engine.getProperty('voices')[7]
-engine.setProperty('voice', voice_selection.id)
 
 
 
 def record(ask=False):
+
     with sr.Microphone() as source:
+
+        rec.pause_threshold = 1.0
+        rec.adjust_for_ambient_noise(source, duration=1)
+
         if ask:
-            engine.say(ask)
-            engine.runAndWait()
+            speak(ask)
+            
         audio = rec.listen(source)
         voice_data = ''
         try:
             voice_data = rec.recognize_google(audio)
+            print(voice_data)
         except sr.UnknownValueError:
-            engine.say('Sorry, I didn\'t get that')
-            engine.runAndWait()
+            speak('Sorry, I didn\'t get that')
+            
         except sr.RequestError:
-            engine.say('Sorry, I\'m not working right now')
-            engine.runAndWait()
+            speak('Sorry, I\'m not working right now')
+            
 
     return voice_data
 
 
+def speak(audio_text):
+    text_to_speech = gTTS(audio_text)
+    rand_num = random.randint(1, 100000)
+    audio_file_name = 'audio_file_' + str(rand_num) + '.mp3'
+    text_to_speech.save(audio_file_name)
+    playsound.playsound(audio_file_name)
+    print(audio_text)
+    os.remove(audio_file_name)
+
+
 def respond(voice_data):
     if 'what is your name' in voice_data:
-        engine.say('My name is Mac')
-        engine.runAndWait()
+        speak('My name is Mac')
+
+    if 'how are you' in voice_data:
+        speak('I\'m well, thank you. How are you?')
+        response = record()
+        if 'well' or 'good' in response:
+            speak('I\'m glad to hear that. How can I help you?')
+        elif 'bad' or 'not' in response:
+            speak('Sorry to hear that. How can I help you today?')
+        else:
+            speak('Ok. How can I help you?')
+
+    if 'what can you' in voice_data:
+        speak('I can do a few basic things like tell you the time and date, search the internet, or find directions.')
+        
     if 'what time' in voice_data:
-        engine.say(ctime()[11:])
-        engine.runAndWait()
-    if (('what is the date' or 'what date is today') in voice_data) and not('tomorrow' or 'yesterday' in voice_data):
-        engine.say(ctime()[0:11])
-        engine.runAndWait()
+        speak(ctime()[11:])
+        
+    if 'what is date' in voice_data:
+        speak(ctime()[0:11])
+        
     if 'search' in voice_data:
         search = record('What do you want to search for?')
         url = 'https://google.com/search?q=' + search
         webbrowser.get().open(url)
-        engine.say('Here is what I found for ' + search)
-        engine.runAndWait()
+        speak('Here is what I found for ' + search)
+        
     if 'directions' in voice_data:
         start = record('Where do you want directions from?')
         start_search = start.replace(' ', '+')
         end = record('Where do you want directions to?')
         end_search = end.replace(' ', '+')
-    
         url = 'https://www.google.com/maps/dir/' + start_search + '/' + end_search
         webbrowser.get().open(url)
-        engine.say('Here are directions from ' + start + ' to ' + end)
-        engine.runAndWait()
+        speak('Here are directions from ' + start + ' to ' + end)
+        
     if 'stop' in voice_data:
         exit()
 
 
 time.sleep(1)
-engine.say('Hi, how can I help?')
-engine.runAndWait()
+speak('Hi, how can I help?')
+
 while 1:
     voice_data = record()
     respond(voice_data)
