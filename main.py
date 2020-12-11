8ba36488cef8f127405a0c7a27062d7ad6518f3e
Kevin Joseph <68094449+kjoseph141@users.noreply.github.com>
2020-12-10 20:47:04 -0500

    Fixed errors in commands and added more commands

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import speech_recognition as sr
 import time
 from time import ctime
 import webbrowser
-import pyttsx3 as tts
 import playsound
 import os
 import random
@@ -17,7 +16,7 @@ def record(ask=False):
 
     with sr.Microphone() as source:
 
-        rec.pause_threshold = 1.0
+        rec.pause_threshold = 1
         rec.adjust_for_ambient_noise(source, duration=1)
 
         if ask:
@@ -29,7 +28,7 @@ def record(ask=False):
             voice_data = rec.recognize_google(audio)
             print(voice_data)
         except sr.UnknownValueError:
-            speak('Sorry, I didn\'t get that')
+            print('No valid command detected. Waiting for command...')
             
         except sr.RequestError:
             speak('Sorry, I\'m not working right now')
@@ -49,15 +48,21 @@ def speak(audio_text):
 
 
 def respond(voice_data):
+
+    if ('OK Mac' in voice_data) or ('hey Mac' in voice_data):
+        speak('Hi, how can I help?')
+
     if 'what is your name' in voice_data:
-        speak('My name is Mac')
+        speak('My name is Mac. What\'s your name?')
+        name = record()
+        speak('Hi ' + name + '. How can I help you?')
 
     if 'how are you' in voice_data:
         speak('I\'m well, thank you. How are you?')
         response = record()
-        if 'well' or 'good' in response:
+        if ('well' in response) or ('good' in response):
             speak('I\'m glad to hear that. How can I help you?')
-        elif 'bad' or 'not' in response:
+        elif ('bad' in response) or ('not' in response):
             speak('Sorry to hear that. How can I help you today?')
         else:
             speak('Ok. How can I help you?')
@@ -68,7 +73,7 @@ def respond(voice_data):
     if 'what time' in voice_data:
         speak(ctime()[11:])
         
-    if 'what is date' in voice_data:
+    if 'what is date' in voice_data and (not ('tomorrow' in voice_data)):
         speak(ctime()[0:11])
         
     if 'search' in voice_data:
@@ -85,6 +90,15 @@ def respond(voice_data):
         url = 'https://www.google.com/maps/dir/' + start_search + '/' + end_search
         webbrowser.get().open(url)
         speak('Here are directions from ' + start + ' to ' + end)
+
+    if 'YouTube' in voice_data:
+        search = record('What do you want to search on YouTube?')
+        url = 'https://youtube.com/search?q=' + search
+        webbrowser.get().open(url)
+        speak('Here is what I found for ' + search)
+
+    if 'open tab' in voice_data:
+        webbrowser.open_new_tab('https://google.com/')
         
     if 'stop' in voice_data:
         exit()
